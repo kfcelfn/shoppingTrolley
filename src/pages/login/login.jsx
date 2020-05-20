a9c6bc75c1f79d53e24868bb60c5b759c975d903
cx964443 <1277867740@qq.com>
2020-05-20 21:51:35 +0800

    登录

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class login extends Component {
+    render() {
+        return (
+            <div className="login">
+                123
+            </div>
+        );
+    }
+}
+
+export default login;
