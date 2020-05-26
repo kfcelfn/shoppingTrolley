506dbb10dce0993736bd6cc9a06437dc0ee168b9
kfcelfn <595330682@qq.com>
2020-05-26 15:52:50 +0800

    '路由替换成config'

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,10 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Error, Nav, Order, Goods } from './assembly';
 
-import '@/utils/rem'
-import '@/styles/index.less';
-import '@/utils/reset.css'  
+const routes = [
+  { path: '/404', component: Error },
+  { path: '/order', component: Order },
+  { path: '/Goods', component: Goods },
+  { path: '/', component: Nav },
+]
 
-export default class Router extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch> 
-          <Route path='/404' component={Error} />     
-          <Route path='/order' component={Order} />
-          <Route path='/goods' component={Goods} />
-          <Route path='/' component={Nav} />     
-        </Switch>
-      </BrowserRouter>
-    )
-  }
-}
+export default routes
